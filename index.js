488bda904fda5d9f8be6ba136a6efe46d1d38cb5
mtraba <126421244+mtraba@users.noreply.github.com>
2023-02-26 17:56:11 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+document.getElementById("boton").onclick = function () {
+    console.log("Schlumberger (2005-2008)");
+    document.getElementById("experiencia1").innerHTML = "Implementacion de Sistemas de calidad en el armado de ESP - Ingeniero de Campo - Ingeniero de Aplicaciones"
+    
+    ;  
+    
+}
+ 
+document.getElementById("boton1").onclick = function () {
+    console.log("Axion Energy (2008-2023)");
+    document.getElementById("experiencia2").innerHTML = "Ingeniero de Producción de 2008 a 2012 - Ingeniero de Reservorios de 2012 a 2020 - Especialista en Waterflooding de 2020 a 2023"; 
+    
+   
+}
+
+
